f29d16e56e9cf80df9d97fa8f807d366ce296553
Michael Miller <pfxmichaelm@gmail.com>
2015-02-12 10:17:08 -0700

    2/12 am edits

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+	$('#add-items').keyup(function() {
+		console.log($(this).val());
+	}).keyup(function(k) {
+		if(k.which==13) {
+			console.log($(this).val());
+			item = $(this).val();
+			$('ul').prepend('<li>' + item + '</li>');
+			$(this).val('');
+		};
+	})
 
     $('ul').on('click', 'li', function() {
     	console.log('click li');
     })
-    $('add-items').on('click', 'add-items', function() {
-    	console.log('click add-items');
-    })
-    $('ul').on('enter', 'add-items', function() {
-    	$('ul').prepend("<li>Prepended items</li>");
-    })
+
 });
