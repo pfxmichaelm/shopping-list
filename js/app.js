5838a76b3ca8dcc7412fb65be85b55d62e308561
Michael Miller <pfxmichaelm@gmail.com>
2015-02-12 15:34:08 -0700

    2/12 pm edits

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
+	$('#add-items').keyup(function() {
+		console.log($(this).val());
+	}).keyup(function(k) {
+		if(k.which==13) {
+			console.log($(this).val());
+			item = $(this).val();
+			cb = $('<input type="checkbox" value="1" />').prepend(item);
+			$('ul').prepend('<li>' + cb + item + '</li>');
+			$(this).val('');
+			/*$('<input type="checkbox" value="1" />').prependTo('li');*/
+		};
+	})
 
     $('ul').on('click', 'li', function() {
     	console.log('click li');
+    	var $ctrl = $('li').attr({ type: 'checkbox', name: 'chk'}).addClass("chk");
+    	$('list-area').append($ctrl);
     })
-    $('add-items').on('click', 'add-items', function() {
-    	console.log('click add-items');
-    })
-    $('ul').on('enter', 'add-items', function() {
-    	$('ul').prepend("<li>Prepended items</li>");
-    })
+
 });
